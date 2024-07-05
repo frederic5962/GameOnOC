dd536e1094a1f6a3aa8d438f1538d68cfd3c9757
Fred jeune DEV en devenir <fredstessy@outlook.fr>
2024-07-05 11:28:24 +0200

    correction W3C erreur

--- javascript/modal.js
+++ javascript/modal.js
@@ -1,23 +1,25 @@
  function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
   }
-}
+
+
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modalClose = document.querySelector(".close");
 
-// launch modal event
+// launch modal event(fermeture X)
 modalBtn.forEach((btn) => { btn.addEventListener('click', () => modalbg.style.display = "block" )});
 modalClose.addEventListener('click', () => modalbg.style.display = "none");
 
 // launch modal form
-function launchModal() {
+ function launchModal() {
   modalbg.style.display = "block";
 }
 
